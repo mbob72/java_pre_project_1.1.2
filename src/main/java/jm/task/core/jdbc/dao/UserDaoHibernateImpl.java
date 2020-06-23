d66c2b31ab68b1447f0fd6ba946a456ad907831d
Михаил Бобрышев <mbob72@gmail.com>
2020-06-23 16:37:16 +0300

    for hybernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,6 +4,12 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
+import jm.task.core.jdbc.util.UtilHybernate;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
@@ -12,17 +18,30 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        // because of 'create' strategy
+        dropUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (UtilHybernate.CloseableSession session = new UtilHybernate
+                .CloseableSession(UtilHybernate.getSessionFactory().openSession())) {
+            session.getSession().createQuery("DELETE FROM User").executeUpdate();
+        } catch (Exception e) { }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (UtilHybernate.CloseableSession session = new UtilHybernate
+                .CloseableSession(UtilHybernate.getSessionFactory().openSession())) {
+            Session _session = session.getSession();
+            Transaction trx = _session.beginTransaction();
+            User user = new User(name, lastName, age);
+            _session.save(user);
+            trx.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
@@ -32,11 +51,16 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (UtilHybernate.CloseableSession session = new UtilHybernate
+                .CloseableSession(UtilHybernate.getSessionFactory().openSession())) {
+            Criteria criteria = session.getSession().createCriteria(User.class);
+            return (List<User>) criteria
+                    .list();
+        } catch (Exception e) { return null;}
     }
 
     @Override
     public void cleanUsersTable() {
-
+        dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.Connection;
@@ -13,32 +14,32 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         return  userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.cleanUsersTable();
     }
 }
