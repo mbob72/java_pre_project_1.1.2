1ab27d77a65275a3d92737a7dfd964c9d57ca2cd
Михаил Бобрышев <mbob72@gmail.com>
2020-06-23 12:02:20 +0300

    use abstract interface for typing dao instance

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -13,32 +13,32 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         return  userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
     }
 }
